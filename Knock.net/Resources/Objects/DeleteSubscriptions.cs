b7a5892d3bda138bb9b470e6a9a0f5e9d74a7dc6
Juan Manuel Azambuja <juan@mimiquate.com>
2023-06-06 16:05:31 -0300

    feat(kno-3770): add subscription endpoints support (#13)
    * feat(kno-3770): add missing user and object listing endpoints
    
    * feat(kno-3770): add object subscription support
    
    * chore: code review comments

---
+++ Knock.net/Resources/Objects/DeleteSubscriptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Knock
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Params to delete subscriptions
+    /// </summary>
+    public class DeleteSubscriptions : BaseOptions
+    {
+        [JsonProperty("recipients")]
+        public List<object> Recipients { get; set; }
+    }
+}
---
+++ Knock.net/Resources/Objects/ObjectSubscription.cs
@@ -0,0 +1,42 @@
+﻿namespace Knock
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Contains information about a Knock Object record.
+    /// </summary>
+    public class ObjectSubscription
+    {
+
+        /// <summary>
+        /// The object that the recipient is subscribed to
+        /// </summary>
+        [JsonProperty("object")]
+        public Object Object { get; set; }
+
+        /// <summary>
+        /// The object that the recipient is subscribed to
+        /// </summary>
+        [JsonProperty("recipient")]
+        public object Recipient { get; set; }
+
+        /// <summary>
+        /// The data associated
+        /// </summary>
+        [JsonProperty("properties")]
+        public Dictionary<string, object> Properties { get; set; }
+
+        /// <summary>
+        /// The inserted at timestamp of the subscription
+        /// </summary>
+        [JsonProperty("inserted_at")]
+        public string InsertedAt { get; set; }
+
+        /// <summary>
+        /// The updated date for this object (as an ISO8601 datetime string)
+        /// </summary>
+        [JsonProperty("updated_at")]
+        public string UpdatedAt { get; set; }
+    }
+}
